21d69f34928d7f02c1cdaa5d4b0be5f48450014a
Joao Pedro Eidam <jpeidam15@gmail.com>
2020-10-26 19:34:44 -0300

    inicio

---
+++ lib/model/user.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class User {
+  final String nome;
+  // ignore: non_constant_identifier_names
+  final String RA;
+
+  const User({
+    @required this.nome,
+    // ignore: non_constant_identifier_names
+    @required this.RA,
+  });
+}
